22413d073a4db5b4f4ba5974b154256128ec3d6b
Jirateep Tatip <57070018@kmitl.ac.th>
2018-01-22 16:30:52 +0700

    get values score_proj html to view

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
      url(r'^$', views.home, name='home'),
      url(r'^scoreproj/', views.scoreproj, name='scoreproj'),
      url(r'^scoreposter/', views.scoreposter, name='scoreposter'),
+     url(r'^update_scoreproj/', views.update_scoreproj, name='update_scoreproj'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from database_management.models import Project
+from django.db.models import Max
 import logging
 
 log = logging.getLogger('django.db.backends')
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.StreamHandler())
+this_year = Project.objects.all().aggregate(Max('proj_years'))['proj_years__max']
+list_column = ['สื่อการนำเสนอ','การนำเสนอ','การตอบคำถาม','รายงาน','การค้นคว้า','การวิเคราะห์และออกแบบ','ปริมาณงาน','ความยากง่าย','คุณภาพของงาน']
 
 @login_required(login_url="login/")
 def home(request):
@@ -13,10 +16,23 @@ def home(request):
 
 @login_required(login_url="login/")
 def scoreproj(request):
-    queryset = Project.objects.all()
-    list_column = ['สื่อการนำเสนอ','การนำเสนอ','การตอบคำถาม','รายงาน','การค้นคว้า','การวิเคราะห์และออกแบบ','ปริมาณงาน','ความยากง่าย','คุณภาพของงาน']
-    return render(request,"scoreproj.html",{'Projectset':queryset, 'column_name':list_column,'range':range(11)})
+    queryset = Project.objects.filter(proj_years=this_year)
+    lis_select = []
+    for i in range(len(list_column)-1):
+        lis_select.append('select_option'+str(i))
+    return render(request,"scoreproj.html",{'Projectset':queryset, 'column_name':list_column[:len(list_column)-1],'range':range(1,11), 'len_col':lis_select})
 
 @login_required(login_url="login/")
 def scoreposter(request):
     return render(request,"scoreposter.html")
+
+@login_required(login_url="login/")
+def update_scoreproj(request):
+    if request.method == 'POST':
+        proj_selected = request.POST.get("data_proj", None)
+        lis_selected = []
+        for i in range(len(list_column)-1):
+            selected_option = request.POST.get("select_option"+str(i), None)
+            lis_selected.append(int(selected_option))
+        
+    return render(request,"update_scoreproj.html")
