e75131cea0059b31d93486cf11a5ccd941250ab3
Jirateep Tatip <57070018@kmitl.ac.th>
2018-03-28 15:58:23 +0700

    set up docker 60%

--- Project/settings.py
+++ Project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -24,7 +25,7 @@ SECRET_KEY = '66981n9uombkzs)ebdt_4cx@-0k_i&atg(h)cm30(=nj-sk34y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # AUTHENTICATION_BACKENDS = ("django_python3_ldap.auth.LDAPBackend",)
 AUTHENTICATION_BACKENDS = ("django_python3_ldap.auth.LDAPBackend",'django.contrib.auth.backends.ModelBackend',)
@@ -162,6 +163,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+MYSQL_ROOT_PASSWORD = 'root'
 
 # DATABASES = {
 #     'default': {
@@ -211,6 +213,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 LOGIN_REDIRECT_URL = '/'
 
